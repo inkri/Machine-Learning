0619562a9d86a0ca5cfa19d3ae4aa44c9e94c4d8
inkri <inkrijaiswal@gmail.com>
2018-01-19 15:16:03 +0530

    OK

--- Classification/FinalrunESE.R
+++ Classification/FinalrunESE.R
@@ -1,16 +1,17 @@
-##Loading libraries:
-library(usdm) # VIF
-library(mice) # Missing Value Imputation
-library(VIM) # Missing Data Plot
-library(lattice) # Diagnosis of Imputed Data
-library(corrplot) # Correlation Plot
-library(PerformanceAnalytics) # Univariate and Bivariate Analysis
-library(moments) # Skewness
-library(car) #OutlierTest
-library(Metrics) #rmse,etc
+
+##Step 1: Loading libraries:
+library(usdm) 
+library(mice) 
+library(VIM) 
+library(lattice) 
+library(corrplot) 
+library(PerformanceAnalytics) 
+library(moments) 
+library(car) 
+library(Metrics) 
 library(randomForest)
 
-##Load Data:
+##Step 2: Load Data:
 setwd("C:/Users/abhishek.b.jaiswal/Desktop/DataScience/Kaggle_github/Electrolysia supplies electricity")
 ECtrain=read.csv("train.csv")
 ECtest=read.csv("test.csv")
@@ -19,20 +20,19 @@ names(ECtrain)
 
 
 
-##Data conversion and cleaning:
+##Step 3: Data cleaning:
 names(ECtrain)
-ECtest$electricity_consumption=0
-ECtrain$data="train"
-ECtest$data="test"
+ECtest$electricity_consumption=1
+ECtrain$dataset="train"
+ECtest$dataset="test"
 compl=rbind.data.frame(ECtrain,ECtest)
 
 
-
+#Dividing Datetime column:
 compl$year = substr(compl$datetime, 1,4)
 compl$month = substr(compl$datetime, 6,7)
 compl$date = substr(compl$datetime, 9,10)
 compl$hour= substr(compl$datetime, 12,13)
-
 compl$year=as.numeric(compl$year)
 compl$month=as.numeric(compl$month)
 compl$date=as.numeric(compl$date)
@@ -41,27 +41,43 @@ compl$hour=as.numeric(compl$hour)
 str(compl)
 
 
-#Data Rearrange
+#Dataset Rearranging:
 names(compl)
 compl=compl[,c(1,2,3,4,5,6,7,10,11,12,13,8,9)]
 
+##Outlier checking:
+boxplot(compl[,-c(1,2,7,8,9,10,11,12,13)])
+
+##Outlier Treatment:
+i=NA
+j=c(3:6)
+
+for (i in j){
+  sd=sd(compl[,i])
+  xbar=mean(compl[,i])
+  uc=xbar + (3 * sd)
+  lc=xbar - (3 * sd)
+  rol=xbar + (6 * sd)
+  lol=xbar - (6 * sd)
+  compl=compl[!compl[,i] > rol,]
+  compl=compl[!compl[,i] < lol,]
+  compl[compl[,i] > uc,i]=uc
+  compl[compl[,i] < lc,i]=lc
+}
+
+##Outlier checking:
+boxplot(compl[,-c(1,2,7,8,9,10,11,12,13)])
+
+#Separating Dataset:
 ECtrain=compl[compl$data=="train",]
 ECtest=compl[compl$data=="test",]
-ECtrain$data=NULL
-ECtest$data=NULL
+ECtrain$dataset=NULL
+ECtest$dataset=NULL
 ECtest$electricity_consumption=NULL
 ECtrain=ECtrain[,-c(1,2)]
 
-#Data Split in Train and Test
-set.seed(524)
-index=sample(1:nrow(ECtrain), round(0.8 * nrow(ECtrain)))
-training=ECtrain[index,]
-testing=ECtrain[-index,]
-
-
 
 ##Correlation:
-
 corr=cor(ECtrain[-5], use = "complete.obs")
 corrplot(corr, type = "upper", order = "original", 
          tl.col = "black", tl.srt = 45)
@@ -69,35 +85,9 @@ corrplot(corr, type = "upper", order = "original",
 ##Missing values:
 sum(is.na(ECtrain))
 
-##Outlier Treatment:
-i=NA
-j=c(1:4,6:10)
-
-for (i in j){
-  sd=sd(ECtrain[,i])
-  xbar=mean(ECtrain[,i])
-  uc=xbar + (3 * sd)
-  lc=xbar - (3 * sd)
-  rol=xbar + (6 * sd)
-  lol=xbar - (6 * sd)
-  ECtrain=ECtrain[!ECtrain[,i] > rol,]
-  ECtrain=ECtrain[!ECtrain[,i] < lol,]
-  ECtrain[ECtrain[,i] > uc,i]=uc
-  ECtrain[ECtrain[,i] < lc,i]=lc
-}
 
 
-rm(i)
-rm(j)
-rm(lc)
-rm(lol)
-rm(rol)
-rm(sd)
-rm(uc)
-rm(xbar)
-rm(compl)
-
-##Random Forest Model:
+##Step 4: Random Forest Model:
 model=randomForest(electricity_consumption~., data = ECtrain)
 model
 
@@ -107,27 +97,28 @@ library(caret)
 varImp(model)
 varImpPlot(model,type=2)
 
-# Prediction on Training Data:
+##Step 5:Prediction:
 pred=predict(model, ECtrain)
 rmse(ECtrain$electricity_consumption, pred)
 plot(ECtrain$electricity_consumption,pred)
 
-##Tuning:
+##Step 6:Tuning:
 #Error rate of RF
 plot(model)  #Tree:200
 #Tune RF Model, mtry
 t1=tuneRF(ECtrain[,-10],ECtrain[,10],stepFactor=0.5,plot=TRUE,ntreeTry=200,trace=TRUE,improve=0.05)
-#mtry = 3  OOB error = 2971.393
-#mtry = 6 	OOB error = 2615.622
-#mtry = 12 	OOB error = 2610.655
+#mtry = 6 	OOB error = 3030.046
 model2=randomForest(electricity_consumption~.,data=ECtrain,ntree=200,mtry=6,importance=TRUE,proximity=TRUE)
 model2
 
 
-#Prediction on ECtrain Data by model2:
-predtest=predict(model2, testing)
-rmse(testing$electricity_consumption, predtest)
-plot(testing$electricity_consumption,predtest)
+#Prediction on ECtrain and ECtest Data by model2:
+predtrain=predict(model2, ECtrain)
+predtest=predict(model2, ECtest)
+rmse(ECtrain$electricity_consumption, predtest)
+plot(ECtrain$electricity_consumption,predtest)
+rmse(ECtest$electricity_consumption, predtest)
+plot(ECtest$electricity_consumption,predtest)
 
 
 # Creating Submission File:
